84191a2397bfe88851fc9d590e227cbf58468444
alvi2496 <alvi.utsab@gmail.com>
2018-05-17 22:20:44 +0600

    add active_admin.scss to vendor/assets/stylesheets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.assets.precompile += %w[active_admin.scss active_admin.js]
 end
