b7fb1d6d36fe6abda13d05cf20702c65f22f8449
alvi <alvi.mahadi@bdipo.com>
2018-05-20 14:47:34 +0600

    refactor code

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,29 @@
 class StudentsController < ApplicationController
+
+  def new
+    @student = Student.new
+  end
+
   def create
     @student = Student.new(student_params)
     if @student.save
       session[:student_id] = @student.id
       redirect_to new_address_path
     else
-      flash[:alert] = 'Student was not saved successfully'
+      render :new
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update_attributes(student_params)
+      redirect_to new_address_path
+    else
+      render :new
     end
   end
 
